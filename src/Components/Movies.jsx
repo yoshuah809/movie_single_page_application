51f7cdaebb16cbf9295615769791f1498b1e24b8
Josue Garcia <yoshuah809@gmail.com>
2021-12-21 10:56:32 -0600

    added the movies api connection and pulled date from c#

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Movie from "./Movie";
 
 const Movies = () => {
-	const movies = [
-		{
-			title: "Terminator",
-			genre: "Accion",
-			director: "James Cameron",
-			year: 2000,
-		},
-		{
-			title: "Ratatuille",
-			genre: "Adventure",
-			director: "Will Smith",
-			year: 2011,
-		},
-		{
-			title: "Avenger",
-			genre: "Accion",
-			director: "John Wayne",
-			year: 2015,
-		},
-	];
+	const [movies, SetMovies] = useState([]);
+	// const movies = [
+
+	// 	{
+	// 		title: "Terminator",
+	// 		genre: "Accion",
+	// 		director: "James Cameron",
+	// 		year: 2000,
+	// 	},
+	// 	{
+	// 		title: "Ratatuille",
+	// 		genre: "Adventure",
+	// 		director: "Will Smith",
+	// 		year: 2011,
+	// 	},
+	// 	{
+	// 		title: "Avenger",
+	// 		genre: "Accion",
+	// 		director: "John Wayne",
+	// 		year: 2015,
+	// 	},
+	// ];
+
+	const LoadMovies = () => {
+		fetch("http://localhost:5000/api/Movie")
+			.then((response) => response.json())
+			.then((data) => SetMovies(data));
+	};
+
 	return (
 		<div>
 			<h2>Movies</h2>
-			<div>Movies Details</div>
+			<button onClick={() => LoadMovies()}>Load Movies</button>
 
 			{movies.map((movie) => (
 				<Movie data={movie} />
